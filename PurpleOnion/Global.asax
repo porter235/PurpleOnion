cd73ec66e0e3cfb1ca4bbf149c5cb21f8c844ec5
porter235 <31319959+porter235@users.noreply.github.com>
2017-10-15 22:01:00 -0400

    Add project files.

---
+++ PurpleOnion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PurpleOnion.MvcApplication" Language="C#" %>
---
+++ PurpleOnion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PurpleOnion.Startup))]
+namespace PurpleOnion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
