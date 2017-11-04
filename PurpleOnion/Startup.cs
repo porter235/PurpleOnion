63b87a57c495e711cdb2bcd691ecadffc19d454a
porter235 <31319959+porter235@users.noreply.github.com>
2017-11-04 19:48:45 -0400

    Role Registration

--- PurpleOnion/Startup.cs
+++ PurpleOnion/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using PurpleOnion.Models;
 
 [assembly: OwinStartupAttribute(typeof(PurpleOnion.Startup))]
 namespace PurpleOnion
@@ -9,6 +12,55 @@ namespace PurpleOnion
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUsers();
         }
+        //In this method we will create ddefault User roles and Admin user for login
+        private void createRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //In Startup I am creating first Admin Role and creating a default Admin User
+            if (!roleManager.RoleExists("Admin"))
+            {
+                //first we create Admin role
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Addmin super user who will maintain the website
+                var user = new ApplicationUser();
+                user.UserName = "Gabe";
+                user.Email = "porter235@live.marshall.edu";
+
+                string userPWD = "Admin";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+            //creating Employee role
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+            //creating customer role
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+        }
+
     }
 }
