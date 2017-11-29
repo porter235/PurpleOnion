08bb8046a9bc62615a1ffa0e6ccb35d353004a71
porter235 <31319959+porter235@users.noreply.github.com>
2017-11-28 21:24:35 -0500

    Jquery and Inventory Model added

---
+++ PurpleOnion/Models/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PurpleOnion.Models
+{
+    public class Inventory
+    {
+        public String Name { get; set; }
+        public String Brand { get; set; }
+        public String Type { get; set; }
+        public Stock Stock { get; set; }
+        public String Price { get; set; }  
+    }
+    public enum Stock
+    {
+        Low = 1,
+        Medium,
+        High
+    }
+}
