73ff28113813640d2020dafb5e8186f4e390f820
Jason Means <means41@live.marshall.edu>
2017-11-29 16:55:41 -0500

    Added inventory model and controller

--- PurpleOnion/Models/Inventory.cs
+++ PurpleOnion/Models/Inventory.cs
@@ -7,16 +7,24 @@ namespace PurpleOnion.Models
 {
     public class Inventory
     {
+        public int Id { get; set; }
         public String Name { get; set; }
         public String Brand { get; set; }
-        public String Type { get; set; }
+        public Type Type { get; set; }
         public Stock Stock { get; set; }
         public String Price { get; set; }  
     }
     public enum Stock
     {
-        Low = 1,
+        Low,
         Medium,
         High
+    }
+
+    public enum Type
+    {
+        Produce,
+        Jarred,
+        Boxed
     }
 }
