a6efc03b7ded08f3e38b547f116e4a3430cbe111
porter235 <31319959+porter235@users.noreply.github.com>
2017-12-04 00:01:22 -0500

    Re-seeded

--- PurpleOnion/Migrations/Configuration.cs
+++ PurpleOnion/Migrations/Configuration.cs
@@ -1,6 +1,11 @@
+
 namespace PurpleOnion.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using PurpleOnion.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -12,8 +17,45 @@ namespace PurpleOnion.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+
         protected override void Seed(PurpleOnion.Models.ApplicationDbContext context)
         {
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "User" });
+
+            if (!context.Users.Any(t => t.UserName == "porter235@live.marshall.edu"))
+            {
+                var user = new ApplicationUser { UserName = "porter235@live.marshall.edu", Email = "porter235@live.marshall.edu" };
+                userManager.Create(user, "passW0rd!");
+
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Admin" });
+                context.SaveChanges();
+
+                userManager.AddToRole(user.Id, "Admin");
+            }
+            if (!context.Users.Any(t => t.UserName == "purpleonionstore@gmail.com"))
+            {
+                var user = new ApplicationUser { UserName = "purpleonionstore@gmail.com", Email = "purpleonionstore@gmail.com" };
+                userManager.Create(user, "passW0rd!");
+
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Employee" });
+                context.SaveChanges();
+
+                userManager.AddToRole(user.Id, "Employee");
+            }
+            if (!context.Roles.Any(r => r.Name == "User"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = manager.FindByName("Admin");
+                if (role == null)
+                {
+                    role = new IdentityRole("Admin");
+                    manager.Create(role);
+                }
+            }
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
