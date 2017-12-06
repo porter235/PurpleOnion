0e52d48ee02dd5de3247e448952152c48877207c
porter235 <31319959+porter235@users.noreply.github.com>
2017-12-05 23:22:23 -0500

    Initial cart submission

---
+++ PurpleOnion/Models/CartSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PurpleOnion.Models
+{
+    public class CartSystem
+    {
+        public int Id { get; set; }
+        public String Email { get; set; }
+        public List<String> Items {get; set;}
+
+    }
+}
